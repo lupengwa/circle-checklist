d21c19391ca3eb6c1346b2268505b21d824c389e
lupengwa <steve.wang.apple@gmail.com>
2024-02-07 21:44:11 -0800

    Added Checklist components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
-import Example from "./CircularProgressBar";
+import Example from "./components/CircularProgressBar";
+import TodoList from "./components/TodoList";
 
 function App() {
     const percentage = 90;
 
   return (
     <div className="App">
+        <div className='todo-app'>
+            <TodoList />
+        </div>
       <Example percentage={percentage}></Example>
     </div>
   );
